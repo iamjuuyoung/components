e6cb1c803f5185e401d9c5da238102894303448e
leejuyoung <summer94@krafton.com>
2021-11-11 22:43:37 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 
 
 import KTable from "./KTable/KTable.vue";
-import KTableRow from "./KTableRow/KTableRow.vue";
 import Task from "./Task/Task.vue";
-export { KTable, KTableRow, Task };
-console.log('test')
+export { KTable, Task };
